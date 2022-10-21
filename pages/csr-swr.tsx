0d618d03819e2c18c56119f6287a9317a16c5716
Andrzej Zelman <azelman@whitelabelcoders.com>
2022-10-21 16:11:36 +0200

    fetcher function

--- pages/csr-swr.tsx
+++ pages/csr-swr.tsx
@@ -1,12 +1,7 @@
 import useSWR from 'swr'
+import fetcher from '../functions/fetcher'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
-const fetcher = async () => {
-  const response = await fetch('https://api.spacexdata.com/v3/missions')
-  const data = await response.json()
-  return data
-}
 
 function CsrSWR() {
   const { data, error } = useSWR('missions', fetcher)
